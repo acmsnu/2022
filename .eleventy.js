f211c6934b4feb52311eb1f6a9b44882c80a561d
Akshit Garg <garg.akshit@gmail.com>
2022-11-03 20:27:45 +0530

    feat: add people
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
+const fs = require("fs/promises");
+const path = require("path");
+
 const markdownIt = require("markdown-it");
+const yaml = require("js-yaml");
 
 module.exports = (config) => {
   config.addPassthroughCopy("src/static");
+
+  config.addCollection("people", async (_) => {
+    const dir = await fs.readdir("./people");
+    const people = await Promise.all(
+      dir.map(async (file) => {
+        const name = path.join("./people/", file);
+        const content = (await fs.readFile(name)).toString("utf8");
+        const loaded = yaml.load(content);
+
+        return {
+          name: loaded.name || "",
+          line: loaded.line || "",
+        };
+      })
+    );
+
+    return people;
+  });
 
   const md = new markdownIt({
     html: true,
