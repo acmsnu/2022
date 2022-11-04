5302cfd55c9461b5fda5da6ad4940c4d8abde47a
Akshit Garg <garg.akshit@gmail.com>
2022-11-04 15:31:50 +0530

    feat: add invalid format handling
    Signed-off-by: Akshit Garg <garg.akshit@gmail.com>

--- .eleventy.js
+++ .eleventy.js
@@ -9,19 +9,25 @@ module.exports = (config) => {
 
   config.addCollection("people", async (_) => {
     const dir = await fs.readdir("./people");
-    const people = await Promise.all(
-      dir.map(async (file) => {
-        const name = path.join("./people/", file);
-        const content = (await fs.readFile(name)).toString("utf8");
-        const loaded = yaml.load(content);
+    const people = (
+      await Promise.all(
+        dir.map(async (file) => {
+          try {
+            const name = path.join("./people/", file);
+            const content = (await fs.readFile(name)).toString("utf8");
+            const loaded = yaml.load(content);
 
-        return {
-          name: loaded.name || "",
-          line: loaded.about || "",
-          github: loaded.github || "",
-        };
-      })
-    );
+            return {
+              name: loaded.name || "",
+              line: loaded.about || "",
+              github: loaded.github || "",
+            };
+          } catch {
+            return undefined;
+          }
+        })
+      )
+    ).filter((el) => el !== undefined);
 
     return people;
   });
